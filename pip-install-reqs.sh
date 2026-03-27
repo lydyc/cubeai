79398a5d3cf2e94d6661b2189d72b23304200645
root <root@localhost.localdomain>
2026-03-27 15:42:15 +0800

    dupdate

--- pip-install-reqs.sh
+++ pip-install-reqs.sh
@@ -5,4 +5,3 @@ pip3 install -r requirements.txt
 pip3 install --upgrade pip -i https://pypi.tuna.tsinghua.edu.cn/simple
 pip3 install -i https://pypi.tuna.tsinghua.edu.cn/simple uwsgi Flask psutil requests opencv-python-headless threadpool configparser nvidia-ml-py
 pip install pycryptodome -i https://pypi.tuna.tsinghua.edu.cn/simple
-pip3 install --no-cache-dir torch --index-url https://download.pytorch.org/whl/cu121
